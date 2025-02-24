11fc90a28e891a75fae086df37941b8089d6160e
mgkiminseoul <41205027+mgkiminseoul@users.noreply.github.com>
2025-02-24 16:17:18 +0900

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,7 @@ def pytest_addoption(parser):
         default="default_device",
         help="디바이스 식별자"
     )
+"""
+conftest.py를 작성하는 대표적인 이유중 하나는 fixture를 이용해 테스트 코드들의 공통된 설정을 중복 코드 없이 conftest.py 내에서 하기 위함입니다.
+혹시 코드 내에서 중복된 설정이 있다면 fixture 데코레이터를 활용하는 것을 추천드립니다.
+"""
